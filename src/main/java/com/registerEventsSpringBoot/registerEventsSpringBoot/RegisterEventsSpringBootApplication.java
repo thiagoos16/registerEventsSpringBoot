aeaee30f6315893a77c4387237351f4591606698
thiagometal <thiagometal.fne@gmail.com>
2018-04-09 23:19:04 -0400

    primeiro commit

---
+++ src/main/java/com/registerEventsSpringBoot/registerEventsSpringBoot/RegisterEventsSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.registerEventsSpringBoot.registerEventsSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterEventsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterEventsSpringBootApplication.class, args);
+	}
+}
