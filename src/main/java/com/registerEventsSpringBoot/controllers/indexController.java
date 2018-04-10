a290a572fad3ee42742283507dff0deaa2bd5f1e
thiagometal <thiagometal.fne@gmail.com>
2018-04-10 17:57:40 -0400

    efetua ajuste na organizacao das pastas

--- src/main/java/com/registerEventsSpringBoot/controllers/indexController.java
+++ src/main/java/com/registerEventsSpringBoot/controllers/indexController.java
@@ -1,4 +1,4 @@
-package com.registerEventsSpringBoot.registerEventsSpringBoot;
+package com.registerEventsSpringBoot.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
