6dbd9a0fe43af331f8a416687f5ac87755f8722d
thiagometal <thiagometal.fne@gmail.com>
2018-04-11 19:58:31 -0400

    adiciona metodo para efetuar a listagem de eventos

--- src/main/java/com/registerEventsSpringBoot/controllers/EventoController.java
+++ src/main/java/com/registerEventsSpringBoot/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.registerEventsSpringBoot.models.Evento;
 import com.registerEventsSpringBoot.repository.EventoRepository;
@@ -25,5 +26,13 @@ public class EventoController {
 		er.save(evento);
 		
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index"); //vai renderizar o index.html
+		Iterable<Evento> eventos = er.findAll(); //Pois será carregado uma lista de Eventos vindo do banco
+		mv.addObject("eventos", eventos); //enviando a lista de eventos para a view
+		return mv;
 	}
 }
