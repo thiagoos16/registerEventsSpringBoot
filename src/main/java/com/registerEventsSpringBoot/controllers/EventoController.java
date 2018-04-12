aadc936b15dbad94ec76aaf17c172767aae600ad
thiagometal <thiagometal.fne@gmail.com>
2018-04-12 15:43:38 -0400

    adiciona ajustes para salvar e lista convidados

--- src/main/java/com/registerEventsSpringBoot/controllers/EventoController.java
+++ src/main/java/com/registerEventsSpringBoot/controllers/EventoController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.registerEventsSpringBoot.models.Convidado;
 import com.registerEventsSpringBoot.models.Evento;
+import com.registerEventsSpringBoot.repository.ConvidadoRepository;
 import com.registerEventsSpringBoot.repository.EventoRepository;
 
 @Controller
@@ -16,6 +18,9 @@ public class EventoController {
 	@Autowired
 	private EventoRepository er;
 	
+	@Autowired
+	private ConvidadoRepository cr;
+	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/form";
@@ -23,7 +28,6 @@ public class EventoController {
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
 	public String form(Evento evento) {
-		
 		er.save(evento);
 		
 		return "redirect:/cadastrarEvento";
@@ -37,11 +41,21 @@ public class EventoController {
 		return mv;
 	}
 	
-	@RequestMapping("/{codigo}")
-	public ModelAndView detalhesEvento(@PathVariable long codigo) { //@PathVariable pega o que é passado como parâmetro
+	@RequestMapping(value="/{codigo}", method=RequestMethod.GET)
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) { //@PathVariable pega o que é passado como parâmetro
 		ModelAndView mv = new ModelAndView("evento/detalhes");
 		Evento evento = er.findByCodigo(codigo);
 		mv.addObject("evento", evento);
+		Iterable<Convidado> convidados = cr.findByEvento(evento); 
+		mv.addObject("convidados", convidados);
 		return mv;
-	} 
+	}
+	
+	@RequestMapping(value="/{codigo}", method=RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+		return "redirect:/{codigo}";
+	}
 }
---
+++ src/main/java/com/registerEventsSpringBoot/repository/ConvidadoRepository.java
@@ -0,0 +1,10 @@
+package com.registerEventsSpringBoot.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.registerEventsSpringBoot.models.Convidado;
+import com.registerEventsSpringBoot.models.Evento;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
+	Iterable<Convidado> findByEvento(Evento evento);
+}
